df0f7e6a999a8e62674a8be9b0dd4c67e2de4944
kalyug18 <117590896+kalyug18@users.noreply.github.com>
2026-02-07 00:32:08 +0530

    Add pattern printing method in Pattern8.java
    Implement a method to print a specific pattern of stars and spaces.

---
+++ Patterns/Pattern8.java
@@ -0,0 +1,42 @@
+
+*********
+ ******* 
+  *****  
+   ***   
+    *   
+
+class Solution {
+    			
+    public void pattern1(int N) {
+        
+        for (int i=0; i < N; i++) 
+        
+        {
+            
+            for (int j=0; j < i ; j++) 
+            
+            {
+              System.out.print(' ');
+            }
+            
+            for(int j=0; j<2*N-(2*i + 1);j++)
+           
+            {
+              System.out.print('*');
+            }
+            
+              for(int j=0;j<i ;j++)
+            {
+              System.out.print(' ');
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int N = 5; 
+        sol.pattern1(N); 
+    }
+}
