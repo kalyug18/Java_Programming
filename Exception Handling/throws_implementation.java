2c2a85e69309e99f31f1b0c9101f333f771d9ceb
kalyug18 <117590896+kalyug18@users.noreply.github.com>
2026-02-21 15:30:00 +0530

    Enhance readFile method with explanatory comments
    Added comments to explain the throws implementation in readFile method.

--- Exception Handling/throws_implementation.java
+++ Exception Handling/throws_implementation.java
@@ -3,8 +3,10 @@
 import java.io.*;
 
 public class ThrowsExample {
-    public static void readFile() throws FileNotFoundException {
-        FileInputStream fis = new FileInputStream("nonexistentfile.txt");
+    public static void readFile() throws FileNotFoundException {         //This is a warning sign. It tells Java: "This method might cause a file error. 
+                                                                         //I'm not going to fix it here; whoever calls me must handle it." This is known as "ducking" the exception
+          
+        FileInputStream fis = new FileInputStream("nonexistentfile.txt"); //Since "nonexistentfile.txt" doesn't exist, this line will immediately create (throw) a FileNotFoundException
     }
 
     public static void main(String[] args) {
