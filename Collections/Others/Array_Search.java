c946de449cfc7aee09ce97c473bb80082fb7d783
kalyug18 <117590896+kalyug18@users.noreply.github.com>
2026-03-07 19:39:57 +0530

    Add search method to find index of an element
    Implement linear search method in Array_Search.java

---
+++ Collections/Others/Array_Search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int arr[], int x) {
+        // code here
+        
+        
+        for (int i=0 ;i < arr.length;i++)
+        {
+            if ( x == arr[i] )
+            
+            System.out.println("Index is: ");
+            return i;
+            
+        }
+         
+            return -1;
+    }
+}
